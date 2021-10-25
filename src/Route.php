8ff0b665cd15d9e3227a1e501d0ae6ec3f875b35
DarkFrost <m.astudillo.toledo@gmail.com>
2021-10-25 02:05:40 -0300

    Add front routes

--- src/Route.php
+++ src/Route.php
@@ -2,15 +2,24 @@
 /**
  * Route front
  */
+
+use App\Http\Middleware\VerifyCsrfToken;
+
 if(sc_config('WebpayPlus')) {
 Route::group(
     [
         'prefix'    => 'plugin/webpayplus',
+        'middleware' => SC_FRONT_MIDDLEWARE,
         'namespace' => 'App\Plugins\Payment\WebpayPlus\Controllers',
     ],
     function () {
         Route::get('index', 'FrontController@index')
         ->name('webpayplus.index');
+        Route::get('process-order', 'FrontController@processOrder')
+        ->name('webpayplus.processOrder'); 
+        Route::any('return/{orderId}', 'FrontController@return')
+        ->withoutMiddleware(VerifyCsrfToken::class)
+        ->name('webpayplus.return');
     }
 );
 }
