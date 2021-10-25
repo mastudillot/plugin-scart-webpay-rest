7ec862c21df9c5c913d1e583b50695a66503c78c
DarkFrost <m.astudillo.toledo@gmail.com>
2021-10-25 02:07:01 -0300

    Add Webpay model

--- src/Models/PluginModel.php
+++ src/Models/PluginModel.php
@@ -3,11 +3,13 @@
 namespace App\Plugins\Payment\WebpayPlus\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class PluginModel extends Model
 {
     public $timestamps    = false;
-    public $table = '';
+    public $table = 'tbk_webpay_transactions';
     protected $connection = SC_CONNECTION;
     protected $guarded    = [];
 
@@ -18,6 +20,22 @@ class PluginModel extends Model
 
     public function installExtension()
     {
+        if (!Schema::hasTable($this->table)) {
+            Schema::create($this->table, function (Blueprint $table) {
+                $table->increments('id');
+                $table->integer('order_id');
+                $table->integer('amount');
+                $table->string('token', 100)->unique();
+                $table->string('session_id', 100)->nullable();
+                $table->string('status', 50);
+                $table->longText('transbank_response')->nullable();
+                $table->string('transbank_status', 50)->nullable();
+                $table->string('transbank_product', 50);
+                $table->string('transbank_environment', 50)->nullable();
+                $table->timestamps();
+            });
+        }
+
         return ['error' => 0, 'msg' => 'install success'];
     }
     
---
+++ src/Models/WebpayTransaction.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Plugins\Payment\WebpayPlus\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WebpayTransaction extends Model
+{
+    use HasFactory;
+
+    const STATUS_INITIALIZED = 'initialized';
+    const STATUS_FAILED = 'failed';
+    const STATUS_ABORTED_BY_USER = 'aborted_by_user';
+    const STATUS_APPROVED = 'approved';
+
+    const PRODUCT_WEBPAY_PLUS = 'webpay_plus';
+    const PRODUCT_WEBPAY_ONECLICK = 'webpay_oneclick';
+    
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'tbk_webpay_transactions';
+}
