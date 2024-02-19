b003c0a29f726e3f1228debb4d6edcc0e67ba101
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2024-02-19 17:20:15 -0300

    chore: update build script for s-cart 8.1

--- package.sh
+++ package.sh
@@ -1,39 +1,40 @@
 #!/bin/sh
 
 #Script for create the plugin artifact
-echo "tag: $TAG"
 
 if [ "$TAG" = "" ]
 then
    TAG='1.0.0'
 fi
+echo "tag: $TAG"
 
 OUTPUT_DIR="output"
 SDK_DIR="src/TransbankSDK"
-FILE1="src/config.json"
+CONFIG_PLUGIN_FILE="config.json"
 OUTPUT_PLUGIN_DIR="WebpayPlus"
+PLUGIN_FILE="plugin-scart-webpay-rest-$TAG.zip"
 
+# Install plugin dependencies
 cd "$SDK_DIR" || exit
 composer install --no-dev
 composer update --no-dev
 cd ../../
 
-sed -i.bkp "s/\"1.0.0\"/\"${TAG#"v"}\"/g" "$FILE1"
-
-PLUGIN_FILE="plugin-scart-webpay-rest-$TAG.zip"
-
+# Create content
 mkdir -p "$OUTPUT_DIR/$OUTPUT_PLUGIN_DIR"
+cp -r src/* "$OUTPUT_DIR/$OUTPUT_PLUGIN_DIR"
+cd "$OUTPUT_DIR/$OUTPUT_PLUGIN_DIR" || exit
 
-cp -r public "$OUTPUT_DIR/$OUTPUT_PLUGIN_DIR"
-cp -r src "$OUTPUT_DIR/$OUTPUT_PLUGIN_DIR"
+# Change version to config file
+sed -i.bkp "s/\"1.0.0\"/\"${TAG#"v"}\"/g" "$CONFIG_PLUGIN_FILE"
 
-cd "$OUTPUT_DIR" || exit
-zip -FSr $PLUGIN_FILE $OUTPUT_PLUGIN_DIR -x "$OUTPUT_PLUGIN_DIR/src/$FILE1.bkp"
 cd ../
 
-cp "$FILE1.bkp" "$FILE1"
-rm "$FILE1.bkp"
-rm -R "$OUTPUT_DIR/$OUTPUT_PLUGIN_DIR"
+# Create zip
+zip -FSr $PLUGIN_FILE $OUTPUT_PLUGIN_DIR -x "$OUTPUT_PLUGIN_DIR/$CONFIG_PLUGIN_FILE.bkp"
+
+# Delete plugin content
+rm -R "$OUTPUT_PLUGIN_DIR"
 
 echo "Plugin version: $TAG"
 echo "Plugin file: $PLUGIN_FILE"
