4f48be0030960ba4321e7d6b613a236864200d29
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2024-02-20 22:54:09 -0300

    refactor: remove comments from provider

--- src/Provider.php
+++ src/Provider.php
@@ -4,10 +4,3 @@
  */
     $this->loadTranslationsFrom(__DIR__.'/Lang', 'Plugins/Payment/Transbank');
     $this->loadViewsFrom(__DIR__.'/Views', 'Plugins/Payment/Transbank');
-
-    if (sc_config('Transbank')) {
-      // Revisar que se puede hacer
-      // $this->mergeConfigFrom(
-      //     __DIR__.'/config.php', 'key_define_for_plugin'
-      // );
-    }
